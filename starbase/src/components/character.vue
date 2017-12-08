a2d00cf00120b3e94355673770917a8908050672
phonghm <huynhminhphong@live.com>
2017-12-08 16:30:57 +0700

    new versions

---
+++ starbase/src/components/character.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        {{character}}
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['id'],
+  data(){
+      return {
+          character: {}
+      }
+  },
+  methods: {
+      getData(id){
+          var url = `https://swapi.co/api/people/${id}`;
+          console.log(url);
+          fetch(url,{
+              method: 'GET'
+          }).then(response => response.json()).then(json => this.character = json)
+      }
+  },
+  created() {
+      this.getData(this.id);
+  }
+}
+</script>
+
